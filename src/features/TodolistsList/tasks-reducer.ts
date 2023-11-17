13b475ac9cef44ac1ed23e2aaa17779beab4c45d
Diana Lipanova <altere2804@gmail.com>
2023-11-17 20:58:41 +0100

    feat: add "pre-typed" version of createAsyncThunk

--- src/features/TodolistsList/tasks-reducer.ts
+++ src/features/TodolistsList/tasks-reducer.ts
@@ -2,24 +2,25 @@ import { TaskPriorities, TaskStatuses, TaskType, todolistsAPI, UpdateTaskModelTy
 import { AppThunk } from "app/store";
 import { appActions } from "app/app-reducer";
 import { handleServerAppError, handleServerNetworkError } from "utils/error-utils";
-import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import { todolistsActions } from "features/TodolistsList/todolists-reducer";
 import {clearTasksAndTodolists} from "common/actions/common.actions";
+import {createAppAsyncThunk} from "utils/create-app-async-thunk";
 
-const fetchTasks = createAsyncThunk<{tasks: TaskType[], todolistId: string}, string, {rejectValue: unknown}>('tasks/fetchTasks',
-    async (todolistId: string, thunkAPI) => {
-        const {dispatch, rejectWithValue} = thunkAPI
-        try {
-            dispatch(appActions.setAppStatus({status: 'loading'}))
-            const res = await todolistsAPI.getTasks(todolistId)
-            const tasks = res.data.items
-            dispatch(appActions.setAppStatus({status: 'succeeded'}))
-            return {tasks, todolistId}
-        } catch (error: any) {
-            handleServerNetworkError(error, dispatch)
-            return rejectWithValue(null)
-        }
-    })
+const fetchTasks = createAppAsyncThunk<{ tasks: TaskType[], todolistId: string }, string>
+('tasks/fetchTasks', async (todolistId, thunkAPI) => {
+    const {dispatch, rejectWithValue} = thunkAPI
+    try {
+        dispatch(appActions.setAppStatus({status: 'loading'}))
+        const res = await todolistsAPI.getTasks(todolistId)
+        const tasks = res.data.items
+        dispatch(appActions.setAppStatus({status: 'succeeded'}))
+        return {tasks, todolistId}
+    } catch (error: any) {
+        handleServerNetworkError(error, dispatch)
+        return rejectWithValue(null)
+    }
+})
 
 const slice = createSlice({
   name: "tasks",
