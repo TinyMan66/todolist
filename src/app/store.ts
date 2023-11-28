0e62c474afd8fd9ee03fab7c07b452d3b937a24f
Diana Lipanova <altere2804@gmail.com>
2023-11-28 21:12:54 +0100

    refactor: update store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,28 +1,22 @@
 import { tasksReducer } from "features/TodolistsList/tasks-reducer";
 import { todolistsReducer } from "features/TodolistsList/todolists-reducer";
-import { AnyAction, combineReducers } from "redux";
-import thunkMiddleware, { ThunkAction, ThunkDispatch } from "redux-thunk";
+import thunkMiddleware from "redux-thunk";
 import { appReducer } from "./app-reducer";
 import { authReducer } from "features/Login/auth-reducer";
 import { configureStore } from "@reduxjs/toolkit";
 
-const rootReducer = combineReducers({
-  tasks: tasksReducer,
-  todolists: todolistsReducer,
-  app: appReducer,
-  auth: authReducer,
-});
-
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    tasks: tasksReducer,
+    todolists: todolistsReducer,
+    app: appReducer,
+    auth: authReducer,
+  },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunkMiddleware),
 });
 
 export type AppRootStateType = ReturnType<typeof store.getState>;
-
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AnyAction>;
-
-export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, AnyAction>;
+export type AppDispatch = typeof store.dispatch;
 
 // @ts-ignore
 window.store = store;
