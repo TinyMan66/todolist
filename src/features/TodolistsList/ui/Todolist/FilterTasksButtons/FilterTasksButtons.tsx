8e08e146901b449f372b732ec955e3f136509660
Diana Lipanova <altere2804@gmail.com>
2023-12-03 22:02:44 +0100

    refactor: change todolist filters functions

--- src/features/TodolistsList/ui/Todolist/FilterTasksButtons/FilterTasksButtons.tsx
+++ src/features/TodolistsList/ui/Todolist/FilterTasksButtons/FilterTasksButtons.tsx
@@ -1,40 +1,37 @@
 import {Button} from "@mui/material";
-import {TodolistDomainType, todolistsActions} from "features/TodolistsList/model/todolists/todolistsSlice";
+import {
+    FilterValuesType,
+    TodolistDomainType,
+    todolistsActions
+} from "features/TodolistsList/model/todolists/todolistsSlice";
 import {useAppDispatch} from "common/hooks";
 
 export const FilterTasksButtons = ({todolist}: Props) => {
     const dispatch = useAppDispatch();
-    const {id, filter} = todolist
 
-    const allClickHandler = () => {
-        dispatch(todolistsActions.changeTodolistFilter({id, filter}));
-    };
-    const activeClickHandler = () => {
-        dispatch(todolistsActions.changeTodolistFilter({id, filter}));
-    };
-    const completedClickHandler = () => {
-        dispatch(todolistsActions.changeTodolistFilter({id, filter}));
-    };
+    const changeTodolistFilterHandler = (filter : FilterValuesType) => {
+        dispatch(todolistsActions.changeTodolistFilter({id: todolist.id, filter}));
+    }
 
     return (
         <>
             <Button
                 variant={todolist.filter === "all" ? "outlined" : "text"}
-                onClick={allClickHandler}
+                onClick={()=>changeTodolistFilterHandler("all")}
                 color={"inherit"}
             >
                 All
             </Button>
             <Button
                 variant={todolist.filter === "active" ? "outlined" : "text"}
-                onClick={activeClickHandler}
+                onClick={()=>changeTodolistFilterHandler("active")}
                 color={"primary"}
             >
                 Active
             </Button>
             <Button
                 variant={todolist.filter === "completed" ? "outlined" : "text"}
-                onClick={completedClickHandler}
+                onClick={()=>changeTodolistFilterHandler("completed")}
                 color={"secondary"}
             >
                 Completed
